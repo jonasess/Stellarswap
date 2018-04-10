7418b127999a55ce958289f5e62660c5a38f52dd
Jonas <jonasess@gmail.com>
2018-04-10 23:30:34 +0100

    rpc configuration

---
+++ include/rpcdaemonpassword.php
@@ -0,0 +1,33 @@
+<?php
+function initconnectionrpc($coincode){
+if($coincode=="BTC"){ 
+	//bitcoin
+	$coinrpcuser='rpcuser';
+	$coinrpcpassword='rpcpassword';
+	$coinrpcip='ip address where daemon is running';
+	$coinrpcport='rpcport';
+}else{
+	//galaxycash
+	if($coincode=="GCH"){
+	$coinrpcuser='rpcuser';
+	$coinrpcpassword='rpcpassword';
+	$coinrpcip='ip address where daemon is running';
+	$coinrpcport='rpcport';
+	}
+	}
+	$coinconnectionrpc = new Bitcoin($coinrpcuser,$coinrpcpassword,$coinrpcip,$coinrpcport);
+	return $coinconnectionrpc;
+}
+function getpassphrase($coincode){
+	if($coincode=="BTC"){ 
+	//bitcoin
+	$passphrase='your passphrase';
+}else{
+	//galaxycash
+	if($coincode=="GCH"){
+	$passphrase='your passphrase';
+	}
+	}
+	return $passphrase;
+}
+?>
